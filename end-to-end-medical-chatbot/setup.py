c4f1c5db8a20a3f34466886e06c8aa9233a1c59d
sujojohn22 <57512441+sujojohn22@users.noreply.github.com>
2024-12-19 09:56:49 -0800

    Add files via upload

---
+++ end-to-end-medical-chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'sujojohn',
+    author_email= 'sujo@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
